47f98f14d0d3550d9fcbf202ffb0256f23182e80
Tony Contratto <kg9ov@kg9ov.org>
2014-02-01 17:49:46 -0600

    Use the new Config class to read our configuration from an XML file. Dynamically load the input and outputs defined in the configuration file.

--- jtwx/src/main/java/org/jtdev/jtwx/Controller.java
+++ jtwx/src/main/java/org/jtdev/jtwx/Controller.java
@@ -1,15 +1,13 @@
 package org.jtdev.jtwx;
 
-import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Observable;
-import java.util.Properties;
-
 import org.apache.commons.daemon.Daemon;
 import org.apache.commons.daemon.DaemonContext;
 import org.apache.commons.daemon.DaemonInitException;
-import org.jtdev.jtwx.input.Davis;
-import org.jtdev.jtwx.output.Slf4jLogger;
-import org.jtdev.jtwx.output.WeatherUndergroundRapidFire;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,51 +20,48 @@ public class Controller extends Observable implements Daemon, Runnable {
 	
 	@Override
 	public void run() {
-		// TODO Add some kind of real config system (commons configuration?)
-		Properties props = new Properties();
+		Config config = Config.load();
+						
+		logger.info("init weather station");
+		WeatherStation ws = initInput(config);
+		if (ws == null) {
+			logger.error("init weather station failed");
+			return;
+		}
+
+		logger.info("connecting to weather station");
 		try {
-			props.load(this.getClass().getClassLoader().getResourceAsStream("jtwx.properties"));
-		} catch (IOException e) {
-			logger.error("Unable to load jtwx.properties", e);
+			ws.connect();
+		} catch (IllegalArgumentException | WeatherStationException e) {
+			logger.error("weather station connect failed", e);
+			return;
 		}
 		
-		logger.info("Init weather station");
-		Davis davis = new Davis("/dev/ttyS0");
-		try {
-			davis.connect();
-		} catch (Exception e) {
-			logger.info("weather station init failed", e);
-			System.exit(1);
+		logger.info("init outputs");
+		List<WeatherReadingObserver> wroList = initOutputs(config);
+		if (wroList == null) {
+			logger.error("init outputs failed");
+			return;
 		}
 		
-		logger.info("Init outputs");
-		try {
-			WeatherReadingObserver wro;
-			wro = new Slf4jLogger();
-			wro.initialize();
+		logger.info("adding observers");
+		for (WeatherReadingObserver wro : wroList) {
 			this.addObserver(wro);
-			wro = new WeatherUndergroundRapidFire();
-			wro.setParameter("wurf.id", props.getProperty("wurf.id"));
-			wro.setParameter("wurf.password", props.getProperty("wurf.password"));
-			wro.initialize();
-			this.addObserver(wro);
-			wro = null;
-		} catch (Exception e) {
-			logger.info("output init failed", e);
 		}
 		
+		logger.info("startup complete: hold on tight, here we go...");
 		while (!shutdown) {
 			long start = System.currentTimeMillis();
 			try {
 				logger.debug("getting weather reading");
-				WeatherReading wr = davis.getWeatherReading();
+				WeatherReading wr = ws.getWeatherReading();
 				logger.debug("notify observers");
 				setChanged();
 				notifyObservers(wr);
 			} catch (Exception e) {
 				logger.info("failed to get weather reading", e);
 			}
-			logger.debug("Elapsed time (ms): " + (System.currentTimeMillis() - start));
+			logger.debug("Elapsed time (ms): {}", (System.currentTimeMillis() - start));
 			try {
 				Thread.sleep(3000);
 			} catch (InterruptedException e) {
@@ -75,7 +70,11 @@ public class Controller extends Observable implements Daemon, Runnable {
 		}
 		
 		logger.info("disconnect weather station");
-		davis.disconnect();
+		try {
+			ws.disconnect();
+		} catch (WeatherStationException e) {
+			logger.error("disconnect weather station failed", e);
+		}
 		logger.info("controller thread exiting");
 	}
 
@@ -98,6 +97,76 @@ public class Controller extends Observable implements Daemon, Runnable {
 	public void stop() throws Exception {
 		this.shutdown = true;
 		controllerThread.join();
+	}
+	
+	private WeatherStation initInput(Config config) {
+		String className = config.input.className;
+		WeatherStation ws = null;
+		
+		try {
+			Class<?> clazz = Class.forName(className);
+			Constructor<?> constructor = clazz.getConstructor();
+			ws = (WeatherStation) constructor.newInstance();
+		} catch (ClassNotFoundException | NoSuchMethodException
+				| SecurityException | InstantiationException
+				| IllegalAccessException | IllegalArgumentException
+				| InvocationTargetException | ClassCastException e) {
+
+			logger.error("Failed to load input class: {}", className, e);
+			return null;
+		}
+		
+		for (Config.Parameter p : config.input.parameters) {
+			try {
+				ws.setParameter(p.param, p.value);
+			} catch (IllegalArgumentException | WeatherStationException e) {
+				logger.error("Input set parameter failed for: {}", p.param, e);
+				return null;
+			}
+		}
+		
+		return ws;
+	}
+	
+	private List<WeatherReadingObserver> initOutputs(Config config) {
+		List<WeatherReadingObserver> wroList = new ArrayList<WeatherReadingObserver>();
+		
+		for (Config.Output output : config.outputs) {
+			WeatherReadingObserver wro;
+			try {
+				Class<?> clazz = Class.forName(output.className);
+				Constructor<?> constructor = clazz.getConstructor();
+				wro = (WeatherReadingObserver) constructor.newInstance();
+			} catch (ClassNotFoundException | NoSuchMethodException
+					| SecurityException | InstantiationException
+					| IllegalAccessException | IllegalArgumentException
+					| InvocationTargetException e) {
+				logger.error("Failed to load output class: {}", output.className, e);
+				return null;
+			}
+
+			if (output.parameters != null) {
+				for (Config.Parameter p : output.parameters) {
+					try {
+						wro.setParameter(p.param, p.value);
+					} catch (Exception e) {
+						logger.error("output {} set parameter failed for: {}", output.className, p.param, e);
+						return null;
+					}
+				}
+			}
+			
+			try {
+				wro.initialize();
+			} catch (Exception e) {
+				logger.error("initialize() failed for {}", output.className, e);
+				return null;
+			}
+			
+			wroList.add(wro);
+		}
+		
+		return wroList;
 	}
 	
 }
