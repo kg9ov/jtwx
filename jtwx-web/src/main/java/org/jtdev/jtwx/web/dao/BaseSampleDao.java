39d559d30b9619d515a2c864351cfd80278fcaab
Tony Contratto <git@kg9ov.org>
2022-04-08 12:42:14 -0500

    Remove jtwx-web project
    Removes the jtwx-web project.  The code is obsolete and not used
    anywhere.  If we want to have a similar service in the future
    then we can create a new, modernized vesion of it from scratch.

--- jtwx-web/src/main/java/org/jtdev/jtwx/web/dao/BaseSampleDao.java
+++
@@ -1,9 +0,0 @@
-package org.jtdev.jtwx.web.dao;
-
-import java.io.Serializable;
-
-public interface BaseSampleDao<T, K extends Serializable> extends GenericDao<T, K> {
-
-	public T findLatest();
-	
-}
--- jtwx-web/src/main/java/org/jtdev/jtwx/web/dao/OutdoorSampleDao.java
+++
@@ -1,7 +0,0 @@
-package org.jtdev.jtwx.web.dao;
-
-import org.jtdev.jtwx.web.domain.OutdoorSample;
-
-public interface OutdoorSampleDao extends BaseSampleDao<OutdoorSample, Long> {
-
-}
